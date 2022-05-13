6fd2e7951d0162383eaedc94ef9e9066102bc895
harold <haroldalvarez800@gmail.com>
2022-05-13 12:01:35 -0500

    harold

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var btnContinuar = document.getElementById('btnContinuar');
+    var email = document.getElementById('usuario');
+    var password = document.getElementById('password');
+    btnContinuar.addEventListener('click',()=>{
+        alert("hola")
+    })
+}
