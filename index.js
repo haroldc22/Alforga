4ea138863917f2cd199b53c83be2e61a51465853
haroldc22 <haroldalvarez800@gmail.com>
2022-05-25 20:55:44 +0200

    koder

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 window.onload = function(){
-    var btnContinuar = document.getElementById('btnContinuar');
-    var email = document.getElementById('usuario');
-    var password = document.getElementById('password');
-    btnContinuar.addEventListener('click',()=>{
-        alert("hola")
+    let btnContinuar = document.getElementById('btnContinuar');
+    let password = document.getElementById('password').value;
+    btnContinuar.addEventListener('click',(e)=>{
+        e.preventDefault();
+        let email = document.getElementById('usuario');
+        let mostrar = document.getElementById('mostrar');
+        mostrar.innerHTML += "<h2 style='display: inline'>Usuario: </h2>" + email.value + <br />;
     })
 }
